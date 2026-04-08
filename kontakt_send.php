965db7ca67408e432d7e6bf239cf6dd3f6fef8a7
PintarBusiness <160147958+PintarBusiness@users.noreply.github.com>
2026-04-08 19:07:21 +0200

    php da bo delal

--- kontakt_send.php
+++ kontakt_send.php
@@ -2,35 +2,36 @@
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $ime = htmlspecialchars($_POST['ime']);
-    $email = htmlspecialchars($_POST['email']);
-    $telefon = htmlspecialchars($_POST['telefon']);
-    $naslov = htmlspecialchars($_POST['naslov']);
-    $sporocilo = htmlspecialchars($_POST['sporocilo']);
+    $ime = htmlspecialchars($_POST['ime'] ?? '');
+    $email = trim($_POST['email'] ?? '');
+    $telefon = htmlspecialchars($_POST['telefon'] ?? '');
+    $naslov = htmlspecialchars($_POST['naslov'] ?? '');
+    $sporocilo = htmlspecialchars($_POST['sporocilo'] ?? '');
 
-    $to = "jure.pintar9@gmail.com";
-    $subject = "Novo sporočilo s spletne strani";
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "<script>
+        alert('Vnesi veljaven email naslov.');
+        window.location.href='kontakt.html';
+        </script>";
+        exit;
+    }
 
-    $message = "
-Ime: $ime
+    $to = "info@pintsite.si";
+    $subject = "Povpraševanje - Lensicenter Gojzd";
 
-Email: $email
+    $message = "Ime: $ime\r\n\r\n";
+    $message .= "Email: $email\r\n\r\n";
+    $message .= "Telefon: $telefon\r\n\r\n";
+    $message .= "Naslov: $naslov\r\n\r\n";
+    $message .= "---------------------\r\n\r\n";
+    $message .= "Sporočilo:\r\n\r\n$sporocilo";
 
-Telefon: $telefon
-
-Naslov: $naslov
-
----------------------
-
-Sporočilo:
-
-$sporocilo
-";
-
-    $headers = "From: no-reply@tvojadomena.si\r\n";
+    $headers = "From: Novo povpraševanje/naročilo <info@pintsite.si>\r\n";
     $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
 
-    if(mail($to, $subject, $message, $headers)){
+    if (mail($to, $subject, $message, $headers)) {
         echo "<script>
         alert('Sporočilo poslano.');
         window.location.href='kontakt.html';
@@ -41,7 +42,5 @@ $sporocilo
         window.location.href='kontakt.html';
         </script>";
     }
-
 }
-
 ?>
